4035bfa34ddce6a37ff13492cc55d9aa9593aacb
Kabuna254 <youngdanny683@gmail.com>
2025-02-26 15:38:21 +0300

    Data types and variables

---
+++ Practical1.py
@@ -0,0 +1,11 @@
+#Variables hold data
+age = 25 #int (integer)
+height = 5.75 #float (decimal number)
+name = "Charlie" #str (string)
+is_student = True #bool (boolean)
+
+#Printing out the values
+print("Name: ", name) #outputs: Name: Charlie
+print("Age: ", age) #outputs: Age: 25
+print("Height: ", height) #outputs: Height: 5.75
+print("Is a student?", is_student) #outputs: Is a student? True
